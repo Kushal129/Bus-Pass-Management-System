95aefa184a0bc6265ada6fe5d3d13dedc9bd4759
kushal129 <21bmiit129@gmail.com>
2023-12-09 14:29:39 +0530

    init

--- main/footer.php
+++ main/footer.php
@@ -9,7 +9,6 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
     <link rel="stylesheet" href="css/footer.css">
 </head>
-c
 <body>
     <div class="footer-clean">
         <footer>
